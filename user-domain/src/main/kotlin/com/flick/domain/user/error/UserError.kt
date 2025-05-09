a883297a8dbb6ec71d7741e9fbd4ac7e22a8a3a8
sehyuk080101 <164876765+sehyuk080101@users.noreply.github.com>
2025-05-10 01:27:11 +0900

    feat: add PERMISSION_DENIED to UserError

--- user-domain/src/main/kotlin/com/flick/domain/user/error/UserError.kt
+++ user-domain/src/main/kotlin/com/flick/domain/user/error/UserError.kt
@@ -6,4 +6,5 @@ import org.springframework.http.HttpStatus
 enum class UserError(override val status: HttpStatus, override val message: String): CustomError {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found"),
     INSUFFICIENT_BALANCE(HttpStatus.BAD_REQUEST, "Insufficient balance"),
+    PERMISSION_DENIED(HttpStatus.FORBIDDEN, "Permission denied"),
 }
