8fbe7be11dbc0fa267bec56b77250f71a6fa5b6f
sehyuk080101 <164876765+sehyuk080101@users.noreply.github.com>
2025-05-11 16:39:06 +0900

    feat: add KafkaConfig

---
+++ admin-service/src/main/kotlin/com/flick/admin/infra/kafka/KafkaConfig.kt
@@ -0,0 +1,34 @@
+package com.flick.admin.infra.kafka
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+
+@Configuration
+class KafkaConfig(
+    @Value("\${spring.kafka.bootstrap-servers}") private val bootstrapServers: String
+) {
+    @Bean
+    fun consumerFactory(): ConsumerFactory<String, String> {
+        val props = HashMap<String, Any>()
+        props[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers
+        props[ConsumerConfig.GROUP_ID_CONFIG] = "admin-group"
+        props[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        props[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        props[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
+
+        return DefaultKafkaConsumerFactory(props)
+    }
+
+    @Bean
+    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, String>()
+        factory.consumerFactory = consumerFactory()
+        return factory
+    }
+}
