832aa9c3a1461c0ea74b3db98e304c79680dea8b
sehyuk080101 <164876765+sehyuk080101@users.noreply.github.com>
2025-05-11 16:21:29 +0900

    feat: add query methods existsByUsername and findAllByOrderByTotalSalesDesc

--- booth-domain/src/main/kotlin/com/flick/domain/booth/repository/BoothRepository.kt
+++ booth-domain/src/main/kotlin/com/flick/domain/booth/repository/BoothRepository.kt
@@ -8,5 +8,7 @@ import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 interface BoothRepository : CoroutineCrudRepository<BoothEntity, Long> {
     suspend fun findByUsername(username: String): BoothEntity?
     suspend fun findByName(name: String): BoothEntity?
-    suspend fun findAllByStatusIn(status: List<BoothStatus>): Flow<BoothEntity>
+    suspend fun existsByUsername(username: String): Boolean
+    fun findAllByStatusIn(status: List<BoothStatus>): Flow<BoothEntity>
+    fun findAllByOrderByTotalSalesDesc(): Flow<BoothEntity>
 }
