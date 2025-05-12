433cc5f419628cb9dd77d8d0aa12cd0aa817399d
sehyuk080101 <164876765+sehyuk080101@users.noreply.github.com>
2025-05-12 17:52:35 +0900

    feat: add UserRoleTypeConverter to WebFluxConfig

--- admin-service/src/main/kotlin/com/flick/admin/infra/webflux/WebFluxConfig.kt
+++ admin-service/src/main/kotlin/com/flick/admin/infra/webflux/WebFluxConfig.kt
@@ -1,6 +1,7 @@
 package com.flick.admin.infra.webflux
 
-import com.flick.admin.domain.booth.BoothStatusConverter
+import com.flick.admin.domain.booth.converter.BoothStatusConverter
+import com.flick.admin.domain.user.converter.UserRoleTypeConverter
 import org.springframework.context.annotation.Configuration
 import org.springframework.format.FormatterRegistry
 import org.springframework.web.reactive.config.WebFluxConfigurationSupport
@@ -10,5 +11,6 @@ class WebFluxConfig : WebFluxConfigurationSupport() {
     override fun addFormatters(registry: FormatterRegistry) {
         super.addFormatters(registry)
         registry.addConverter(BoothStatusConverter())
+        registry.addConverter(UserRoleTypeConverter())
     }
 }
