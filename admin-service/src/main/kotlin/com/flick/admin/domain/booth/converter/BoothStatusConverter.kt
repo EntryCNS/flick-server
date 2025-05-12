71bca51e6cfd2cb8cbf1681ad3166ff575b6a4f6
sehyuk080101 <164876765+sehyuk080101@users.noreply.github.com>
2025-05-12 22:00:54 +0900

    refactor: handle converter exceptions via GlobalErrorWebExceptionHandler

--- admin-service/src/main/kotlin/com/flick/admin/domain/booth/converter/BoothStatusConverter.kt
+++ admin-service/src/main/kotlin/com/flick/admin/domain/booth/converter/BoothStatusConverter.kt
@@ -1,14 +1,10 @@
 package com.flick.admin.domain.booth.converter
 
-import com.flick.common.error.CustomException
 import com.flick.domain.booth.enums.BoothStatus
-import com.flick.domain.booth.error.BoothError
 import org.springframework.core.convert.converter.Converter
 
 class BoothStatusConverter : Converter<String, BoothStatus> {
     override fun convert(source: String): BoothStatus {
-        val status = BoothStatus.entries.find { it.name.equals(source, ignoreCase = true) }
-
-        return status ?: throw CustomException(BoothError.BOOTH_INVALID_STATUS)
+        return BoothStatus.valueOf(source.uppercase())
     }
 }
--- admin-service/src/main/kotlin/com/flick/admin/domain/user/converter/UserRoleTypeConverter.kt
+++ admin-service/src/main/kotlin/com/flick/admin/domain/user/converter/UserRoleTypeConverter.kt
@@ -1,14 +1,10 @@
 package com.flick.admin.domain.user.converter
 
-import com.flick.common.error.CustomException
 import com.flick.domain.user.enums.UserRoleType
-import com.flick.domain.user.error.UserError
 import org.springframework.core.convert.converter.Converter
 
 class UserRoleTypeConverter : Converter<String, UserRoleType> {
     override fun convert(source: String): UserRoleType {
-        val roleType = UserRoleType.entries.find { it.name.equals(source, ignoreCase = true) }
-
-        return roleType ?: throw CustomException(UserError.INVALID_ROLE_TYPE)
+        return UserRoleType.valueOf(source.uppercase())
     }
 }
