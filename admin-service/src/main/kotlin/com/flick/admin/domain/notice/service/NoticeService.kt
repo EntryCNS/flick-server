303d132b3d50f28cfab35156db65c12583731e75
ENCAIL <siwon0876@gmail.com>
2025-05-13 16:34:38 +0900

    feat: add broadcast

--- admin-service/src/main/kotlin/com/flick/admin/domain/notice/service/NoticeService.kt
+++ admin-service/src/main/kotlin/com/flick/admin/domain/notice/service/NoticeService.kt
@@ -1,5 +1,7 @@
-package com.flick.core.domain.notice.service
+package com.flick.admin.domain.notice.service
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.flick.admin.domain.notice.dto.NoticeCreatedEvent
 import com.flick.admin.domain.notice.dto.request.CreateNoticeRequest
 import com.flick.admin.domain.notice.dto.request.UpdateNoticeRequest
 import com.flick.admin.infra.security.SecurityHolder
@@ -8,14 +10,19 @@ import com.flick.core.domain.notice.dto.response.NoticeResponse
 import com.flick.domain.notice.entity.NoticeEntity
 import com.flick.domain.notice.error.NoticeError
 import com.flick.domain.notice.repository.NoticeRepository
+import com.flick.domain.user.repository.UserRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
 
 @Service
 class NoticeService(
     private val noticeRepository: NoticeRepository,
-    private val securityHolder: SecurityHolder
+    private val securityHolder: SecurityHolder,
+    private val objectMapper: ObjectMapper,
+    private val userRepository: UserRepository,
+    private val kafkaTemplate: KafkaTemplate<Any, Any>
 ) {
     suspend fun getNotices(): Flow<NoticeResponse> {
         val notices = noticeRepository.findAll()
@@ -46,18 +53,21 @@ class NoticeService(
 
     suspend fun createNotice(request: CreateNoticeRequest) {
         val adminId = securityHolder.getAdminId()
+        val notice = noticeRepository.save(
+            NoticeEntity(
+                title = request.title,
+                content = request.content,
+                authorId = adminId,
+                isPinned = request.isPinned
+            )
+        )
 
-        noticeRepository.save(NoticeEntity(
-            title = request.title,
-            content = request.content,
-            authorId = adminId,
-            isPinned = false,
-        ))
+        broadcastNotice(notice)
     }
 
-    suspend fun updateNotice(id: Long, request: UpdateNoticeRequest) {
+    suspend fun updateNotice(noticeId: Long, request: UpdateNoticeRequest) {
         val adminId = securityHolder.getAdminId()
-        val notice = noticeRepository.findById(id)
+        val notice = noticeRepository.findById(noticeId)
             ?: throw CustomException(NoticeError.NOTICE_NOT_FOUND)
 
         if (notice.authorId != adminId) {
@@ -65,16 +75,16 @@ class NoticeService(
         }
 
         val updatedNotice = notice.copy(
-            title = request.title,
-            content = request.content
+            title = request.title ?: notice.title,
+            content = request.content ?: notice.content,
         )
 
         noticeRepository.save(updatedNotice)
     }
 
-    suspend fun deleteNotice(id: Long) {
+    suspend fun deleteNotice(noticeId: Long) {
         val adminId = securityHolder.getAdminId()
-        val notice = noticeRepository.findById(id)
+        val notice = noticeRepository.findById(noticeId)
             ?: throw CustomException(NoticeError.NOTICE_NOT_FOUND)
 
         if (notice.authorId != adminId) {
@@ -82,5 +92,19 @@ class NoticeService(
         }
 
         noticeRepository.delete(notice)
+    }
+
+    private suspend fun broadcastNotice(notice: NoticeEntity) {
+        val users = userRepository.findAll()
+
+        users.collect {
+            val event = NoticeCreatedEvent(
+                id = notice.id!!,
+                userId = it.id!!,
+                title = notice.title,
+            )
+            val eventJson = objectMapper.writeValueAsString(event)
+            kafkaTemplate.send("notice-created", eventJson)
+        }
     }
 }
